5c2410945163192c8ba3117ca9339d43c151a6f5
varshini <varshinisugavanesh@gmail.com>
2024-09-27 12:26:59 +0530

    added  a string largest odd number  example

--- String-Basic/3-string-larOdd.js
+++ String-Basic/3-string-larOdd.js
@@ -1,15 +1,24 @@
-// find the largest odd number in the given string
+// find the largest odd number digits in the given string
 
 function largestOdd(s) {
-  for (let i = s.length; i >= 0; i--) {
+  // have taken the last index of the string
+  for (let i = s.length - 1; i >= 0; i--) {
+    // if the string contains the odd digit cut until that
     if (parseInt(s[i]) % 2 === 1) {
-      return s.slice(0, i + 1);
+      // excluding the leading zeros
+      let start = 0;
+
+      while (start < i && s[start] === "0") {
+        start++;
+      }
+      // printing the digits of largest odd numbers
+      return s.slice(start, i + 1);
     }
   }
   return "";
 }
 
-const str = "1356479";
-
+// const str = "1356479";
+const str = "002643854";
 const result = largestOdd(str);
 console.log(`Largest of odd number is ${result}`);
