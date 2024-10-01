5d0c6310d9b814157146cf8cbb1aa5f6c30971f1
varshini <varshinisugavanesh@gmail.com>
2024-10-01 16:53:37 +0530

    added hashing highest occ ele prob

--- Hashing-Basic/1-high-occ-ele.js
+++ Hashing-Basic/1-high-occ-ele.js
@@ -1,9 +1,43 @@
 // Highest occurring element in an array
 
-function solution(arr) {
-  console.log(arr);
+function mostFreqElement(arr) {
+  // variable to store the size of an array
+  let n = arr.length;
+
+  // variable to store the maximum freq
+  let maxFreq = 0;
+
+  // variable to store the maximum freq of the ele
+  let maxEle = 0;
+
+  // visited array
+  let visited = new Array(n).fill(false);
+
+  for (let i = 0; i < n; i++) {
+    if (visited[i]) continue;
+
+    let freq = 0;
+
+    for (let j = i; j < n; j++) {
+      if (arr[i] == arr[j]) {
+        freq++;
+        visited[j] = true;
+      }
+    }
+
+    if (freq > maxFreq) {
+      maxFreq = freq;
+      maxEle = arr[i];
+    } else if (freq == maxFreq) {
+      maxEle = Math.min(maxEle, arr[i]);
+    }
+  }
+
+  return maxEle;
 }
 
 const arrEle = [1, 2, 3, 3, 3, 4, 5];
 
-solution(arrEle);
+let result = mostFreqElement(arrEle);
+
+console.log(`The highest occuring element in the array is : ${result}`);
