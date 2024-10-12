14aa92c9a82e0116b34f3b08821eb59f1816bbd5
varshini <varshinisugavanesh@gmail.com>
2024-10-12 19:19:15 +0530

    added a array,  started a intersection problem

---
+++ Array-DSA-LogicBuild/5-intersection.js
@@ -0,0 +1,19 @@
+// Intersection of two sorted arrays
+
+/* 
+Given two sorted arrays nums1 and nums2, 
+return an array containing the intersection of these two arrays.
+
+
+
+The intersection of two arrays is an array 
+where all values are present in both arrays.
+*/
+
+function intersectionOfSortedArray(nums1, nums2) {}
+
+const arr1 = [3, 4, 5, 6, 8, 8],
+  arr2 = [2, 4, 6, 6, 6, 10];
+
+const result = intersectionOfSortedArray(arr1, arr2);
+console.log(`The intersection of two arrays is an array: ${result}`);
