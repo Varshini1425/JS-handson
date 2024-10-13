2f27293e22e2a9c6d33d070625c3e043f5e7b4b9
varshini <varshinisugavanesh@gmail.com>
2024-10-13 11:53:10 +0530

    added an array logic fund, intersection

--- Array-DSA-LogicBuild/5-intersection.js
+++ Array-DSA-LogicBuild/5-intersection.js
@@ -10,7 +10,23 @@ The intersection of two arrays is an array
 where all values are present in both arrays.
 */
 
-function intersectionOfSortedArray(nums1, nums2) {}
+function intersectionOfSortedArray(nums1, nums2) {
+  let ansResult = [];
+  let visited = Array(nums2.length).fill(0);
+
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      if (nums1[i] === nums2[j] && visited[j] === 0) {
+        ansResult.push(nums2[j]);
+        visited[j] = 1;
+        break;
+      } else if (nums2[j] > nums1[i]) {
+        break;
+      }
+    }
+  }
+  return ansResult;
+}
 
 const arr1 = [3, 4, 5, 6, 8, 8],
   arr2 = [2, 4, 6, 6, 6, 10];
